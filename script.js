5bb95e2fc63ecd57687c77d7a3028a3bad13fbfe
Aman Jaiswar <amanjaiswar54@gmail.com>
2026-03-27 10:20:31 +0530

    logictrack

--- script.js
+++ script.js
@@ -14,3 +14,36 @@ $(document).ready(function () {
         $('.nav-links').toggleClass('active');
     });
 })
+$(function () {
+
+    let index = 0;
+    const total = $('.slide').length;
+
+    function updateSlider() {
+        $('.slides').css({
+            transform: 'translateX(-' + (index * 100) + '%)',
+            transition: '0.5s ease'
+        });
+    }
+
+    $('.next').click(function () {
+        index++;
+        if (index >= total) index = 0;
+        updateSlider();
+    });
+
+    $('.prev').click(function () {
+        alert('juywudgtuw')
+        index--;
+        if (index < 0) index = total - 1;
+        updateSlider();
+    });
+
+    // auto slide
+    setInterval(function () {
+        index++;
+        if (index >= total) index = 0;
+        updateSlider();
+    }, 3000);
+
+});
